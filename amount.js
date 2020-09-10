47bd2de3d259191e329df3206d82517bb8a12de8
p_okaywang <p_okaywang@tencent.com>
2020-09-10 11:25:00 +0800

    up

--- amount.js
+++ amount.js
@@ -22,12 +22,12 @@ exports.fen2yuan = function (val) {
 /**
  * 格式化大数为指定的单位，如：12345.67 => 1.23万
  * @param value 需要转换的数字
- * @param decimal 需要保留的小数位个数，默认两位
+ * @param decimal 需要保留的小数位个数，默认两位，切割时遵循四舍五入
  * @param options 额外参数
  */
 exports.toText = function (value, decimal, options) {
     if (decimal === void 0) { decimal = 2; }
-    var _a = options || {}, _b = _a.unit /* 默认单位为空 */, unit = _b === void 0 ? "" : _b /* 默认单位为空 */, _c = _a.level /* 默认转换万以上的数字 */, level = _c === void 0 ? 10000 : _c /* 默认转换万以上的数字 */, _d = _a.strip /* 默认去除多余的尾部0，如1.20万->1.2万 */, strip = _d === void 0 ? true : _d /* 默认去除多余的尾部0，如1.20万->1.2万 */;
+    var _a = options || {}, _b = _a.unit /* 默认单位为空 */, unit = _b === void 0 ? "" : _b /* 默认单位为空 */, _c = _a.baseline /* 默认转换万以上的数字 */, baseline = _c === void 0 ? 10000 : _c /* 默认转换万以上的数字 */, _d = _a.strip /* 默认去除多余的尾部0，如1.20万->1.2万 */, strip = _d === void 0 ? false : _d /* 默认去除多余的尾部0，如1.20万->1.2万 */;
     var val = +value;
     var ret = String(val);
     // 不对异常数据处理
@@ -35,7 +35,7 @@ exports.toText = function (value, decimal, options) {
         return value;
     }
     var absVal = Math.abs(val);
-    if (absVal < Math.pow(10, 4) || absVal < level) {
+    if (absVal < Math.pow(10, 4) || absVal < baseline) {
         ret = val.toFixed(decimal);
     }
     else if (absVal >= Math.pow(10, 4) && absVal < Math.pow(10, 8)) {
@@ -48,39 +48,24 @@ exports.toText = function (value, decimal, options) {
     return ret + unit;
 };
 /**
- * 转换为常用货币展示格式，按千分割数字，如：12345.67 => 12,345.67
+ * 转换为常用货币展示格式，按千分割数字，如：-12345.6 => -12,345.60
+ *
+ * *如果收到异常数据，则会原样返回*
  * @param val 需要转换的数字
- * @param decimal 需要保留的小数位个数，默认2位
- * @param strip 去除结尾多余的0，如12345.00 => 12,345.00，默认去除
+ * @param decimal 需要保留的小数位个数，默认2位，切割时遵循四舍五入
+ * @param strip 是否去除结尾多余的0，如12345.10 => 12,345.1，默认去除
  */
 exports.toCurrency = function (val, decimal, strip) {
     if (decimal === void 0) { decimal = 2; }
-    if (strip === void 0) { strip = true; }
-    val = String(val) || "";
-    var pattern = /^(\-?)(\d+)(\.\d+)?$/, macher = pattern.exec(val);
-    if (macher === null) {
+    if (strip === void 0) { strip = false; }
+    val = String(val) || '';
+    if (!/^(\-?)(\d+)(\.\d+)?$/.exec(val)) {
         return val;
     }
-    var sign = null != macher ? RegExp.$1 || "" : "", integer = null != macher ? RegExp.$2 || "0" : "0", bit = null != macher ? RegExp.$3 || ".00" : ".00", size = integer.length, mod = size > 3 ? size % 3 : 0, tmp = "", start = 0 == mod ? "" : integer.substr(0, mod) + ",", flag = 0;
-    if (decimal == 0) {
-        bit = "";
-    }
-    else if (bit.length >= decimal + 1) {
-        bit = bit.substr(0, decimal + 1);
-    }
-    else {
-        bit = (bit + new Array(decimal + 1 - bit.length + 1).join("0")).substr(0, decimal + 1);
-    }
-    for (var i = mod; i < size; i++) {
-        tmp += integer.charAt(i);
-        flag++;
-        if (flag % 3 == 0 && i < size - 1) {
-            tmp += ",";
-            flag = 0;
-        }
-    }
-    bit = strip ? bit.replace(/\.?0+$/, '') : bit;
-    return sign + start + tmp + bit;
+    var valStr = Number(val).toFixed(decimal);
+    valStr = valStr.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+    valStr = strip ? valStr.replace(/\.?0+$/, '') : valStr;
+    return valStr;
 };
 exports["default"] = {
     fen2yuan: exports.fen2yuan,
