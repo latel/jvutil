c77e1c8b0868868478909f270f666256179fe63b
p_okaywang <p_okaywang@tencent.com>
2020-09-10 11:19:42 +0800

    amount#toCurrency, amount#toText默认不去除小数部分尾部多余的0
    优化测试代码，增加readme使用示例

--- src/amount.ts
+++ src/amount.ts
@@ -22,8 +22,8 @@ export const fen2yuan = (val: number | string): string => {
 interface ToTextOptions {
   /** 单位（股/手/张等） */
   unit?: string;
-  /** 转换最低级别，如需要超过10万才转换 */
-  level?: number;
+  /** 接受转换最低金额，如指定超过10万才处理转换 */
+  baseline?: number;
   /** 是否尽可能的去除尾部多余的0，如1.200张->1.2张，默认去除 */
   strip?: boolean;
 }
@@ -31,7 +31,7 @@ interface ToTextOptions {
 /**
  * 格式化大数为指定的单位，如：12345.67 => 1.23万
  * @param value 需要转换的数字
- * @param decimal 需要保留的小数位个数，默认两位
+ * @param decimal 需要保留的小数位个数，默认两位，切割时遵循四舍五入
  * @param options 额外参数
  */
 export const toText = (
@@ -41,8 +41,8 @@ export const toText = (
 ): string | number => {
   const {
     unit = "" /* 默认单位为空 */,
-    level = 10000 /* 默认转换万以上的数字 */,
-    strip = true /* 默认去除多余的尾部0，如1.20万->1.2万 */,
+    baseline = 10000 /* 默认转换万以上的数字 */,
+    strip = false /* 默认去除多余的尾部0，如1.20万->1.2万 */,
   } = options || {};
   let val: number = +value;
   let ret: string = String(val);
@@ -51,7 +51,7 @@ export const toText = (
     return value;
   }
   let absVal = Math.abs(val);
-  if (absVal < Math.pow(10, 4) || absVal < level) {
+  if (absVal < Math.pow(10, 4) || absVal < baseline) {
     ret = val.toFixed(decimal);
   } else if (absVal >= Math.pow(10, 4) && absVal < Math.pow(10, 8)) {
     ret = (val / 10000).toFixed(decimal) + "万";
@@ -63,47 +63,23 @@ export const toText = (
 };
 
 /**
- * 转换为常用货币展示格式，按千分割数字，如：12345.67 => 12,345.67
+ * 转换为常用货币展示格式，按千分割数字，如：-12345.6 => -12,345.60
+ *
+ * *如果收到异常数据，则会原样返回*
  * @param val 需要转换的数字
- * @param decimal 需要保留的小数位个数，默认2位
- * @param strip 去除结尾多余的0，如12345.00 => 12,345.00，默认去除
+ * @param decimal 需要保留的小数位个数，默认2位，切割时遵循四舍五入
+ * @param strip 是否去除结尾多余的0，如12345.10 => 12,345.1，默认去除
  */
-export const toCurrency = (val: number | string, decimal = 2, strip = true): string => {
-  val = String(val) || "";
-  var pattern = /^(\-?)(\d+)(\.\d+)?$/,
-    macher = pattern.exec(val);
-  if (macher === null) {
+export const toCurrency = (val: number | string, decimal = 2, strip = false): string => {
+  val = String(val) || '';
+  if (!/^(\-?)(\d+)(\.\d+)?$/.exec(val)) {
     return val;
   }
-  var sign = null != macher ? RegExp.$1 || "" : "",
-    integer = null != macher ? RegExp.$2 || "0" : "0",
-    bit = null != macher ? RegExp.$3 || ".00" : ".00",
-    size = integer.length,
-    mod = size > 3 ? size % 3 : 0,
-    tmp = "",
-    start = 0 == mod ? "" : integer.substr(0, mod) + ",",
-    flag = 0;
-  if (decimal == 0) {
-    bit = "";
-  } else if (bit.length >= decimal + 1) {
-    bit = bit.substr(0, decimal + 1);
-  } else {
-    bit = (bit + new Array(decimal + 1 - bit.length + 1).join("0")).substr(
-      0,
-      decimal + 1
-    );
-  }
-  for (var i = mod; i < size; i++) {
-    tmp += integer.charAt(i);
-    flag++;
-    if (flag % 3 == 0 && i < size - 1) {
-      tmp += ",";
-      flag = 0;
-    }
-  }
-  bit = strip ? bit.replace(/\.?0+$/, '') : bit;
-  return sign + start + tmp + bit;
-};
+  let valStr = Number(val).toFixed(decimal);
+  valStr = valStr.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+  valStr = strip ? valStr.replace(/\.?0+$/, '') : valStr;
+  return valStr;
+}
 
 export default {
   fen2yuan,
