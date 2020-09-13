d9f83e53cc912124ebd8bfe7b0bb9933e43b44fa
kevinnwang <kevinnwang@tencent.com>
2020-09-13 23:34:57 +0800

    add scripts

---
+++ dist/math/src/index.d.ts
@@ -0,0 +1,23 @@
+/**
+ * 数字相加
+ */
+export declare const add: (arg1: number | string, arg2: number | string) => number;
+/**
+ * 数字相减
+ */
+export declare const reduce: (arg1: number | string, arg2: number | string) => number;
+/**
+ * 数字相乘
+ */
+export declare const mul: (arg1: number | string, arg2: number | string) => number;
+/**
+ * 数字相除
+ */
+export declare const div: (arg1: number | string, arg2: number | string, acc?: number) => number;
+/**
+ * 取模操作
+ * @param dividend 目标数字
+ * @param divisor 模
+ * @returns 余数
+ */
+export declare const modulo: (dividend: number | string, divisor: number | string) => number;
